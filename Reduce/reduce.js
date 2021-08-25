fa28a35041de3f7801d4924544cfe4c6b2c07c83
kenezeoke <kenezeoke@yahoo.com>
2021-08-25 01:35:12 -0400

    used js method reduce instead of writing my own method

--- Reduce/reduce.js
+++ Reduce/reduce.js
@@ -1,6 +1,18 @@
- function Reduce(array, reducer, initialValue) {
+function Reduce(array, reducer, initialValue) {
   // TODO: write your code here
-    return array.reduce(reducer,initialValue);
+  function record(arr, acc){
+   if (!arr.length) {
+     return acc;
+   }
+   const curr = arr[0];
+   const nextArr = arr.slice(1);
+   const nextAcc = reducer(acc, curr);
+   return record(nextArr, nextAcc);
+   }
+   if (initialValue) {
+     return record(array, initialValue);
+   }
+   return record(array.slice(1), array[0]);
 }
 
 module.exports = Reduce;
